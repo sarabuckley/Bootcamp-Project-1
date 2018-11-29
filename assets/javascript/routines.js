24f2455f181c5cc2ad9fcc076c583fb893776f96
Konenkamp <n0267144@lm.lmig.com>
2018-11-28 20:27:21 -0800

    adding gif ids

--- assets/javascript/routines.js
+++ assets/javascript/routines.js
@@ -7,29 +7,68 @@ var cardioChosen = false;
 var bodyweightChosen = false;
 
 var cardioArray = [
-    {name: "plank",
-     gifID: "KbOsMppYjfO5a",  
-     time: 60,
-     instructions: "Lie face down with forearms on the floor and hands clasped. Extend the legs behind the body and rise up on the toes. Keeping the back straight, tighten the core and hold the position for 30-60 seconds (or as long as you can hang)."
+    {
+        name: "Invisible Jump Rope",
+        gifID: "images/jumprope.gif",
+        time: 60,
+        instructions: "Jumping rope not only enhances your footwork, shoulder strength, and coordination, but also simulates sprinting"
     },
-    {name: "burpee",
-     gifID: "l0IpWJtQ7ksIIL8cM",  
-     time: 60,
-     instructions: "Start out in a low squat position with hands on the floor. Next, kick the feet back to a push-up position, complete one push-up, then immediately return the feet to the squat position. Leap up as high as possible before squatting and moving back into the push-up portion of the show."
+    {
+        name: "Spot Jogging",
+        gifID: "images/invisibleJog.gif",
+        time: 60,
+        instructions: " Lift your knees as high as possible and try kicking your butt. Drive with your arms."
+    },
+
+    {
+        name: "Jumping Jacks",
+        gifID: "images/jumpingJack.gif",
+        time: 60,
+        instructions: ""
+    },
+
+    {
+        name: "Mountain Climber Twist",
+        gifID: "images/mtnClimberTwist.gif",
+        time: 60,
+        instructions: ""
+    },
+
+    {
+        name: "Skaters",
+        gifID: "images/skaters.gif",
+        time: 60,
+        instructions: ""
     },
     
-    {name: "jumpingjack",
-     gifID: "l0IpWJtQ7ksIIL8cM",
-     time: 60,
-     instructions: "Stand up straight with your shoulders back and your pelvis relaxed. Hold your arms at your side with your feet shoulder-width apart, then jump, spreading your legs slightly and extending your arms over your head. As you land, bring your arms back down to your sides. Tuck in your tummy while doing these for toning your lower abs as well."
+    {
+        name: "Squats",
+        gifID: "images/squats.gif",
+        time: 60,
+        instructions: ""
+    },
+
+    {
+        name: "Leg Ups",
+        gifID: "images/legUps.gif",
+        time: 60,
+        instructions: ""
+    },
+
+    {
+        name: "Jumping Lunges",
+        gifID: "images/jumpLunge.gif",
+        time: 60,
+        instructions: ""
     }
 ]
 
 var bodyweightArray = [
-    {name: "lateral squat",
-     gifID: "26BROTgFzjf22acJq",  
-     time: 60,
-     instructions: "Start in a standing position, take your feet out sideways so that they are wider than shoulder width and toes turned out slightly. Reach your hands forwards at shoulder height."
+    {
+        name: "lateral squat",
+        gifID: "images/sideLunge.gif",
+        time: 60,
+        instructions: "Start in a standing position, take your feet out sideways so that they are wider than shoulder width and toes turned out slightly. Reach your hands forwards at shoulder height."
     }
 ]
 
@@ -38,50 +77,50 @@ var bodyweightArray = [
 function displayExerciseGifs() {
 
     //Check type of exercise selected
-    if ($("#customCheck1").prop("checked")){
+    if ($("#customCheck1").prop("checked")) {
         bodyweightChosen = true;
         console.log("bodyweight: " + bodyweightChosen);
     }
 
-    if ($("#customCheck2").prop("checked")){
+    if ($("#customCheck2").prop("checked")) {
         cardioChosen = true;
         console.log("cardio:" + cardioChosen);
     }
-    
+
     // Check length of exercise routine selected
     routineTime = $("#times option:selected").text();
     var routineLength = routineTime.replace(" minutes", "");
-  
-    
-    var queryURL = "http://api.giphy.com/v1/gifs/" + cardioArray[0].gifID +"?api_key=dc6zaTOxFJmzC";
-    
+
+
+    var queryURL = "http://api.giphy.com/v1/gifs/" + cardioArray[0].gifID + "?api_key=dc6zaTOxFJmzC";
+
     // Creating an AJAX call for gif selected from array
     $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
 
-      results = response.data; 
-              
-     // Store info from object returned
-     var imgURL_Still = results.images.fixed_height_still.url;
-     var imgURL_Animate = results.images.fixed_height.url;
-     
-         // Create a div to hold the animal gifs
-         var exerciseDiv = $("<div class='exercise-view'>");
+        results = response.data;
 
-         // Dynamically create image 
-         var image = $("<img>").attr("src", imgURL_Animate);
-         image.addClass("gif");
-         image.attr("data-state", "animate") 
-         image.attr("data-still", imgURL_Still)
-         image.attr("data-animate", imgURL_Animate) 
-       
-         // Add image to div
-         exerciseDiv.append(image);
-         $("#exercise-view").prepend(exerciseDiv);
-    });  
+        // Store info from object returned
+        var imgURL_Still = results.images.fixed_height_still.url;
+        var imgURL_Animate = results.images.fixed_height.url;
+
+        // Create a div to hold the animal gifs
+        var exerciseDiv = $("<div class='exercise-view'>");
+
+        // Dynamically create image 
+        var image = $("<img>").attr("src", imgURL_Animate);
+        image.addClass("gif");
+        image.attr("data-state", "animate")
+        image.attr("data-still", imgURL_Still)
+        image.attr("data-animate", imgURL_Animate)
+
+        // Add image to div
+        exerciseDiv.append(image);
+        $("#exercise-view").prepend(exerciseDiv);
+    });
 }
 
 
@@ -89,4 +128,4 @@ function displayExerciseGifs() {
 
 $("#startBtn").on("click", displayExerciseGifs);
 
-    
+
